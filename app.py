18bb559d9ae974b7d8261edb567b876e3466de59
ochukoerewive <ochukoerewive@gmail.com>
2021-06-20 05:56:03 +0000

    Create the Flask App

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ if os.path.exists("env.py"):
 
 
         if __name__== "__main__":
-            app.run(host=os.environ.get("IP", "0.0.0.0"),
+            app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
